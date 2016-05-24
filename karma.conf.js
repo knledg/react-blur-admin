3ef5f37f67ab106b17928472aed53777f8a65791
Matt Lorey <mattlorey@gmail.com>
2016-05-24 14:49:33 -0700

    Setup test environment (#1)

---
+++ karma.conf.js
@@ -0,0 +1,69 @@
+var path = require('path');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
+      'test/**/*.js'
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      'src/**/*.js': ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'inline-source-map', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: path.resolve(__dirname, 'node_modules'),
+            query: {
+              presets: ['es2015', 'react', 'stage-1']
+            }
+          },
+          {
+            test: /\.json$/,
+            loader: 'json',
+          },
+        ]
+      },
+      externals: {
+        'cheerio': 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+      }
+    },
+
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+
+    plugins: [
+      'karma-webpack',
+      'karma-mocha',
+      'karma-sourcemap-loader',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher'
+    ],
+
+
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015', 'react', 'stage-1']
+      }
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+  })
+};
