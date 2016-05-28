084003c8e623ee56484c53f7b723e7e098bd2301
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-05-28 09:59:39 -0700

    Commit pending tests

---
+++ test/input-test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+
+import { Input } from '../src';
+
+describe('<Input/>', function() {
+  it('Has default props');
+
+  it('Input types');
+
+  it('Input ID');
+
+  it('Input name');
+
+  it('Input className');
+
+  it('Input placeholder');
+
+  it('Input helpLabel');
+
+  it('Input addonLeft');
+
+  it('Input addonRight');
+
+  it('Input hasFeedback');
+
+  it('Input does not have hasFeedback');
+
+  it('Input has autofocus');
+
+  it('Input does not have autofocus');
+
+  it('Input has FeebackIcon');
+
+  it('Input does not have Feedback icon');
+
+  it('Input onValidate');
+
+  it('Input onKeyDown');
+
+  it('Input value');
+
+  it('Input onChange');
+
+  it('Input disabled');
+
+  it('Input is not disabled');
+
+  it('Input is rounded');
+
+  it('Input is not rounded');
+});
+
