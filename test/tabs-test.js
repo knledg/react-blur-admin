e293130f3ae12f6413e1759134380cd05fa1bbc2
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-06-02 14:14:37 -0700

    Tabs Test
    In progress

---
+++ test/tabs-test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+
+import { Tabs } from '../src';
+
+describe('<Tabs/>', function() {
+  it('Has default properties');
+
+  it('Is aligned left');
+
+  it('Is aligned right');
+
+  it('Start tab is being rendered');
+
+  it('Renders children');
+
+  it('Renders tab content');
+
+  it('Has a panel');
+});
