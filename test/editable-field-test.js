52162e2483a7e8279d87ba33763abe69c9a5ad52
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-06-02 14:09:20 -0700

    Editable Field Test
    In Progress

---
+++ test/editable-field-test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+
+import { EditableField } from '../src';
+
+describe('<EditableField/>', function() {
+  it('Has default properties');
+
+  it('Has an onChange function');
+
+  it('Has an onValidate function');
+
+  it('Has a value of text');
+
+  it('Has a value of a component');
+
+  it('Has error is true');
+
+  it('Has error is false');
+
+  it('Has error help label');
+
+});
+
