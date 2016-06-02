79e483b891efe1c608d5ada930c3fa6e306ba61a
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-06-02 14:04:38 -0700

    Pagination Test
    In progress

---
+++ test/pagination-test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import { noop } from 'lodash';
+
+import { Pagination } from '../src';
+
+describe('<Pagination/>', function() {
+//   it('Renders current pages', function() {
+//     const component = shallow(<Pagination onChange={noop} currentPage={1} totalPages={9999}/>);
+//     expect(component.find('ul').length).to.equal(1);
+//   });
+// });
+
+it('Renders total pages');
+
+it('Has onChange function');
+
+it('Is a valid page');
+
+
