0f2141eac41c48b505ae8dabd7ea4349d48194ac
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-06-02 14:02:09 -0700

    Select Test
    In progress

---
+++ test/select-test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import {noop} from 'lodash';
+
+import { Select } from '../src';
+
+it('Default properties');
+
+it('Has a placeholder');
+
+it('Has an onChange function');
+
+it('onChange function is rendered');
+
+it('Has a value of number');
+
+it('Has a value of string');
+
+it('Has a label');
+
+it('Has a text value');
+
+it('Has a component value');
+
+it('is Searchable');
+
+it('Does not have search capabilities');
+
+it('is Open');
+
+it('onToggleOpen is rendered');
