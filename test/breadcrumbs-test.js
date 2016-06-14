86146a084204854e8bfb426337018fdd61f38e54
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-06-14 09:36:12 -0700

    Breadcrumbs Test
    add test

--- test/breadcrumbs-test.js
+++ test/breadcrumbs-test.js
@@ -9,8 +9,13 @@ describe('<Breadcrumb />', function() {
     const component = shallow(<Breadcrumbs />);
     expect(component.find('ul').hasClass('breadcrumb al-breadcrumb')).to.equal(true);
   });
-  it('Renders className', function() {
+  it('className is being rendered', function() {
     const component = shallow(<Breadcrumbs className='testing className'/>);
-    expect(component.find('ul').hasClass('breadcrumb al-breadcrumb')).text().to.contain('testing className');
+    expect(component.find('ul').hasClass('testing className')).to.equal(true);
   });
+});
+
+it('Renders li child components', function() {
+  const component = shallow(<Breadcrumbs>{_.map(_.range(0, 5), (num) => `Child ${num}`)}</Breadcrumbs>);
+  expect(component.find('li')).to.have.length(5);
 });
