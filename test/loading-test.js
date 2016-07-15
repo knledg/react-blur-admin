6365cd91289a1ccaa24eceed386b6fa194923c81
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-07-15 14:00:08 -0700

    Loading Test
    Fixed title

--- test/loading-test.js
+++ test/loading-test.js
@@ -5,7 +5,7 @@ import {expect} from 'chai';
 import { Loading } from '../src';
 
 describe('<Loading/>', function() {
-  it('Is a text type', function() {
+  it('Has class fa fa-spinner fa-spin', function() {
     const component = shallow(<Loading/>);
     expect(component.find('i').hasClass('fa fa-spinner fa-spin')).to.equal(true);
   });
