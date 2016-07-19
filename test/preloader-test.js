121fd80f85e7cec6ed1f947cb451553a01e9d001
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-07-19 07:43:14 -0700

    Preloader Tests
    /

--- test/preloader-test.js
+++ test/preloader-test.js
@@ -5,15 +5,17 @@ import {expect} from 'chai';
 import { Preloader } from '../src';
 
 describe('<Preloader />', function() {
-  describe('Has default props', function() {
-    const component = mount(<Preloader />);
+  const component = mount(<Preloader />);
 
-    it('Contains preloader container', function() {
-      expect(component.find('div').at(0).hasClass('preloader-component-container')).to.equal(true);
-    });
+  it('Contains preloader container', function() {
+    expect(component.find('div.preloader-component-container').length).to.equal(1);
+  });
 
-    it('Contains preloader component', function() {
-      expect(component.find('div').at(1).hasClass('preloader-component')).to.equal(true);
-    });
+  it('Contains preloader component', function() {
+    expect(component.find('div.preloader-component').length).to.equal(1);
+  });
+
+  it('Has a third nested div', function() {
+    expect(component.find('div div div').length).to.equal(3);
   });
 });
