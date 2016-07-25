57f2cf3334a87fc46c10ad215f4c1f3c735bc8be
Ashley Fogarty <ashleyfogarty95@gmail.com>
2016-07-25 07:48:48 -0700

    Tab Component Test (#50)
    Closes #38

---
+++ test/tab-test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {expect} from 'chai';
+import {noop} from 'lodash';
+
+import { Tab } from '../src';
+
+describe('<Tab/>', function() {
+  const component = mount(<Tab title='this is a title' onClick={noop}/>);
+  it('renders an a tag', function() {
+    expect(component.find('a')).to.have.length(1);
+  });
+  it('can pass in a title', function() {
+    expect(component.find('a').text()).to.contain('this is a title');
+  });
+});
+it('Has an onClick change', function() {
+  const onClick = sinon.spy();
+  const component = mount(<Tab onClick={onClick}/>);
+  component.find('a').simulate('click');
+  expect(onClick.calledOnce).to.equal(true);
+});
